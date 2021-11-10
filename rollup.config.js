bf819e07ef0abc5390463c76042a2fd9bbd6120b
= <=>
2021-11-10 17:49:44 +0100

    minify bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 const packageJson = require('./package.json');
 
@@ -30,7 +31,8 @@ const rollupConfig = {
     typescript({ useTsconfigDeclarationDir: true, exclude: ['src/**/*.stories.tsx'] }),
     postcss({
       extensions: ['.css']
-    })
+    }),
+    terser()
   ]
 };
 
